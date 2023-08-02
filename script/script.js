5c7d4b9956db4d96874852fb365f02b68e4978c6
m-sarabi <m.sarabi.jkd@gmail.com>
2023-08-02 18:49:31 +0330

    halved the scale and made the parts more parametric

--- script/script.js
+++ script/script.js
@@ -1,41 +1,55 @@
+const cellSize = 40;
+
+const tailPathD = 'M 40 10 C 35 10 15 10 10 10 C 0 10 0 30 10 30 C 15 30 35 30 40 30';
+
 // snake tail
 const tailSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+tailSVG.style.position = 'absolute';
 const tailPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-tailPath.setAttribute('d', 'M 80 20 L 20 20 C 0 20 0 60 20 60 L 80 60');
+tailPath.setAttribute('d', tailPathD);
 tailPath.setAttribute('fill', 'orange');
 tailPath.setAttribute('stroke', 'black');
 tailSVG.appendChild(tailPath);
 
+const bodyCurvedD = 'M 0 30 C 0 25 0 15 0 10 C 15 10 30 25 30 40 C 25 40 15 40 10 40 C 10 35 5 30 0 30';
+const bodyCurvedStrokeD = 'M 0 10 C 15 10 30 25 30 40 M 10 40 C 10 35 5 30 0 30';
+
 // snake body curved
 const bodyCurveSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+bodyCurveSVG.style.position = 'absolute';
 const bodyCurvePath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-bodyCurvePath.setAttribute('d', 'M 0 60 C 0 46 0 32 0 20 C 30 20 60 50 60 80 C 46.6667 80 33.3333 80 20 80 C 20 70 10 60 0 60');
+bodyCurvePath.setAttribute('d', bodyCurvedD);
 bodyCurvePath.setAttribute('fill', 'orange');
 const bodyCurvePathStroke = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-bodyCurvePathStroke.setAttribute('d', 'M 0 20 C 30 20 60 50 60 80 M 20 80 C 20 70 10 60 0 60');
+bodyCurvePathStroke.setAttribute('d', bodyCurvedStrokeD);
 bodyCurvePathStroke.setAttribute('fill', 'transparent');
 bodyCurvePathStroke.setAttribute('stroke', 'black');
 bodyCurveSVG.appendChild(bodyCurvePath);
 bodyCurveSVG.appendChild(bodyCurvePathStroke);
 
+const bodyStraightD = 'M 0 30 C 0 25 0 15 0 10 C 5 10 35 10 40 10 C 40 15 40 25 40 30 C 35 30 5 30 0 30';
+const bodyStraightStrokeD = 'M 0 10 C 5 10 35 10 40 10 M 40 30 C 35 30 5 30 0 30';
+
 // snake body straight
 const bodyStraightSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+bodyStraightSVG.style.position = 'absolute';
 const bodyStraightPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-bodyStraightPath.setAttribute('d', 'M 0 60 C 0 46.6667 0 33.3333 0 20 C 26.6667 20 53.3333 20 80 20 C 80 33.3333 80 46.6667 80 60 C 53.3333 60 26.6667 60 0 60');
+bodyStraightPath.setAttribute('d', bodyStraightD);
 bodyStraightPath.setAttribute('fill', 'orange');
 const bodyStraightPathStroke = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-bodyStraightPathStroke.setAttribute('d', 'M 0 20 C 26.6667 20 53.3333 20 80 20 M 80 60 C 53.3333 60 26.6667 60 0 60');
+bodyStraightPathStroke.setAttribute('d', bodyStraightStrokeD);
 bodyStraightPathStroke.setAttribute('fill', 'transparent');
 bodyStraightPathStroke.setAttribute('stroke', 'black');
 bodyStraightSVG.appendChild(bodyStraightPath);
 bodyStraightSVG.appendChild(bodyStraightPathStroke);
 
 // snake head
-const headMainD = 'M 0 60 C 6.6667 60 13.3333 60 20 60 C 27 67 33 67 40 60 C 46.6667 60 53.3333 60 60 60 C 80 60 80 20 60 20 C 53.3333 20 46.6667 20 40 20 C 33 13 27 13 20 20 C 13.3333 20 6.6667 20 0 20';
-const headNoseD = 'M 65 30 C 66 32 66 33 65 35 M 65 50 C 66 48 66 47 65 45';
-const headEyesD = 'M 30 20 C 35 20 35 25 30 25 C 26 25 26 20 30 20 M 30 60 C 35 60 35 55 30 55 C 26 55 26 60 30 60';
+const headMainD = 'M 0 30 C 5 30 5 30 10 30 C 15 35 15 35 20 30 C 25 30 25 30 30 30 C 40 30 40 10 30 10 C 25 10 25 10 20 10 C 15 5 15 5 10 10 C 5 10 5 10 0 10';
+const headNoseD = 'M 33 15 C 34 16 34 17 33 18 M 33 25 C 34 24 34 23 33 22';
+const headEyesD = 'M 15 10 C 18 10 18 13 15 13 C 13 13 13 10 15 10 M 15 30 C 18 30 18 27 15 27 C 13 27 13 30 15 30';
 
 const headSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+headSVG.style.position = 'absolute';
 const headGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
 const headMain = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 headMain.setAttribute('d', headMainD);
@@ -50,22 +64,22 @@ const headEyes = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 headEyes.setAttribute('d', headEyesD);
 headEyes.setAttribute('fill', 'white');
 headEyes.setAttribute('stroke', 'black');
-const eyesAnimate = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
-eyesAnimate.setAttribute('attributeName', 'd');
-eyesAnimate.setAttribute('values', 'M 30 20 C 35 20 35 25 30 25 C 26 25 26 20 30 20 M 30 60 C 35 60 35 55 30 55 C 26 55 26 60 30 60;' +
-    'M 30 20 C 35 20 35 25 30 25 C 26 25 26 20 30 20 M 30 60 C 35 60 35 55 30 55 C 26 55 26 60 30 60;' +
-    'M 30 20 C 35 20 35 25 30 25 C 35 25 35 20 30 20 M 30 60 C 35 60 35 55 30 55 C 35 55 35 60 30 60;' +
-    'M 30 20 C 35 20 35 25 30 25 C 26 25 26 20 30 20 M 30 60 C 35 60 35 55 30 55 C 26 55 26 60 30 60');
-eyesAnimate.setAttribute('keyTimes', '0;0.7;0.85;1');
-eyesAnimate.setAttribute('dur', '5s');
-eyesAnimate.setAttribute('repeatCount', 'indefinite');
-headEyes.appendChild(eyesAnimate);
+// const eyesAnimate = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
+// eyesAnimate.setAttribute('attributeName', 'd');
+// eyesAnimate.setAttribute('values', 'M 30 20 C 35 20 35 25 30 25 C 26 25 26 20 30 20 M 30 60 C 35 60 35 55 30 55 C 26 55 26 60 30 60;' +
+//     'M 30 20 C 35 20 35 25 30 25 C 26 25 26 20 30 20 M 30 60 C 35 60 35 55 30 55 C 26 55 26 60 30 60;' +
+//     'M 30 20 C 35 20 35 25 30 25 C 35 25 35 20 30 20 M 30 60 C 35 60 35 55 30 55 C 35 55 35 60 30 60;' +
+//     'M 30 20 C 35 20 35 25 30 25 C 26 25 26 20 30 20 M 30 60 C 35 60 35 55 30 55 C 26 55 26 60 30 60');
+// eyesAnimate.setAttribute('keyTimes', '0;0.7;0.85;1');
+// eyesAnimate.setAttribute('dur', '5s');
+// eyesAnimate.setAttribute('repeatCount', 'indefinite');
+// headEyes.appendChild(eyesAnimate);
 headGroup.appendChild(headMain);
 headGroup.appendChild(headNose);
 headGroup.appendChild(headEyes);
 headSVG.appendChild(headGroup);
 
-document.body.appendChild(headSVG);
+// document.body.appendChild(headSVG);
 
 function rotatePath(svgPath) {
     const parts = svgPath.split(/(?=[MC])/);
@@ -75,10 +89,10 @@ function rotatePath(svgPath) {
         const coords = part.slice(1).trim().split(/\s+/);
         switch (type) {
             case 'M':
-                rotatedPath += `${type} ${-parseFloat(coords[1]) + 80} ${parseFloat(coords[0])} `;
+                rotatedPath += `${type} ${-parseFloat(coords[1]) + cellSize} ${parseFloat(coords[0])} `;
                 break;
             case 'C':
-                rotatedPath += `${type} ${-parseFloat(coords[1]) + 80} ${parseFloat(coords[0])} ${-parseFloat(coords[3]) + 80} ${parseFloat(coords[2])} ${-parseFloat(coords[5]) + 80} ${parseFloat(coords[4])}     `;
+                rotatedPath += `${type} ${-parseFloat(coords[1]) + cellSize} ${parseFloat(coords[0])} ${-parseFloat(coords[3]) + cellSize} ${parseFloat(coords[2])} ${-parseFloat(coords[5]) + cellSize} ${parseFloat(coords[4])}     `;
         }
     }
     return rotatedPath.trim().replace(/  +/, ' ');
